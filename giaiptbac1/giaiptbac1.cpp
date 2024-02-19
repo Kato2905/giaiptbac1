30af1d1764c8f51ffd3160eff1d3314b06ebdfe3
ADMIN <ADMIN@LAB1-MAY29>
2024-02-19 16:34:06 +0700

    Add project files.

---
+++ giaiptbac1/giaiptbac1.cpp
@@ -0,0 +1,29 @@
+// giaiptbac1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	float x;
+	cout << "UNG DUNG GIAI PHUONG TRINH BAC 1\n";
+	cout << "Nhap a:"; cin >> a;
+	cout << "Nhap b:"; cin >> b;
+	if (a == 0)
+	{
+		if (b = 0)
+		{
+			cout << "PT dung voi moi x ~ vo so nghiem\n";
+
+		}
+		else {
+			cout << "PT vo nghiem\n";
+		}
+		cout << "PT vo nghiem\n";
+	}
+	else {
+		x = (float)-b / a;
+		cout << "Ket qua x =" << endl;
+	}
+	return 0; 
+}
